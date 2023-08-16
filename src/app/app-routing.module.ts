983a27214b716e04a351b88e44609f818038aa97
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-16 14:10:53 +0200

    Do app components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+// +import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent}
+  // { path: "home", component: HomeComponent },
+  // { path: "login", component: LoginComponent },
+  // { path: "about", component: AboutComponent },
+  // { path: "contact", component: ContactComponent },
+  // { path: "users", component: UserListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './layout/menu/menu.component';
 import { PublicHeaderComponent } from './components/public-header/public-header.component';
+import { PrivateHeaderComponent } from './components/private-header/private-header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PublicMenuComponent } from './components/public-menu/public-menu.component';
+import { PrivateMenuComponent } from './components/private-menu/private-menu.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { UsersComponent } from './pages/users/users.component';
+import { ProvidersComponent } from './pages/providers/providers.component';
+import { CostumersComponent } from './pages/costumers/costumers.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    PublicHeaderComponent
+    PublicHeaderComponent,
+    PrivateHeaderComponent,
+    FooterComponent,
+    PublicMenuComponent,
+    PrivateMenuComponent,
+    LoginComponent,
+    AboutUsComponent,
+    DashboardComponent,
+    UsersComponent,
+    ProvidersComponent,
+    CostumersComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
