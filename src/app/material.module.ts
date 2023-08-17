1aa4644d1a91b282cae77d428b56909ad06b6b70
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-17 18:13:24 +0200

    Fix npm dependencies

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ], 
+  imports: [CommonModule],
   exports: [],
 })
-export class MaterialModule { }
+export class MaterialModule {}
