7ff4dac290a0025c127b607a381ca68a7238a8ab
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-17 19:44:46 +0200

    1. Move styles menu-public to styles.scss. 2. Do private-menu component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { GalleryComponent } from "./pages/gallery/gallery.component";
+import { CrudComponent } from "./pages/crud/crud.component";
+import { ProfileComponent } from "./pages/profile/profile.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "about", component: AboutUsComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "gallery", component: GalleryComponent },
+  { path: "crud", component: CrudComponent },
+  { path: "profile", component: ProfileComponent },
 ];
 
 @NgModule({
