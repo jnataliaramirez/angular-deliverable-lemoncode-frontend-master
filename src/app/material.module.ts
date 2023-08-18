bdf240011de4650d996cbb2fb73bc776f73896b6
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-18 18:49:08 +0200

    Do login page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,25 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
-  exports: [MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+  ],
 })
 export class MaterialModule {}
