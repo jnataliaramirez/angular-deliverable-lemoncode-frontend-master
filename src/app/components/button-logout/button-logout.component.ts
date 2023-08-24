d20b82518b8f9fd9c6205ab88c3dbb813f641eaa
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-24 19:42:18 +0200

    Do router on button-logout

--- src/app/components/button-logout/button-logout.component.ts
+++ src/app/components/button-logout/button-logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { AuthService } from "src/app/services/auth.service";
 
 @Component({
@@ -7,11 +8,12 @@ import { AuthService } from "src/app/services/auth.service";
   styleUrls: ["./button-logout.component.scss"],
 })
 export class ButtonLogoutComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
   logout(): void {
     this.authService.logout();
+    this.router.navigate(["/home"]);
   }
 }
