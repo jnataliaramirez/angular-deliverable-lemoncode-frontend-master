d93a0405af338a06fc009750046d5a1737374bd4
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-24 18:37:44 +0200

    Show public and private menu according to the user is loget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "angular-deliverable-lemoncode-frontend-master";
+
+  constructor(private authService: AuthService) {}
+
+  isLooged(): boolean {
+    return this.authService.isLooged();
+  }
+
+
 }
