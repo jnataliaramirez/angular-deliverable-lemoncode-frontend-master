4bd895d88f8ef6dc2fe7869c61832e7e089258ac
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-24 19:36:11 +0200

    Fix styles in private-menu

--- src/app/components/button-logout/button-logout.component.ts
+++ src/app/components/button-logout/button-logout.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-button-logout',
-  templateUrl: './button-logout.component.html',
-  styleUrls: ['./button-logout.component.scss']
+  selector: "app-button-logout",
+  templateUrl: "./button-logout.component.html",
+  styleUrls: ["./button-logout.component.scss"],
 })
-export class ButtonLogoutComponent {
+export class ButtonLogoutComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {}
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
